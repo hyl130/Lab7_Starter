75c5275790536ec4dc4a20c42be66e2888373433
Hyunjo <hyl130@ucsd.edu>
2021-11-05 16:50:31 -0700

    debugging

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -196,7 +196,7 @@ function bindEscKey() {
    */
    document.addEventListener('keydown', event => {
      if(event.key == "Escape"){
-      router.navigate('home', true);
+      router.navigate('home', false);
      }
 });}
 
